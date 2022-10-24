3c26d3b53b9cc772e90e1938bef527c5c0fb9030
Pamaforce <1327374483@qq.com>
2022-10-25 00:45:08 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -135,7 +135,7 @@ export default {
       .then((res) => {
         if (res.data.code === "00000") {
           wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
             appId: "wx07c0bd592efbb4f0", // 必填，公众号的唯一标识
             timestamp: res.data.result.timestamp, // 必填，生成签名的时间戳
             nonceStr: res.data.result.nonceStr, // 必填，生成签名的随机串
